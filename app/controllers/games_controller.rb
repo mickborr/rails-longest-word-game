6d6dc34c5e8e5b645ebad9de563520e916023fed
mickael borrajo <mick.borrajo.ael@gmail.com>
2021-02-04 21:32:44 +0100

    new part done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,15 @@
 class GamesController < ApplicationController
+  def new
+    @letters = generate_grid(9)
+  end
+
+  def score
+  end
+
+  private
+
+  def generate_grid(grid_size)
+    # TODO: generate random grid of letters
+    Array.new(grid_size) { ('A'..'Z').to_a.sample }
+  end
 end
